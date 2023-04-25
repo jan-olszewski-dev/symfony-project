334a739d3d591061fec6c5114bcb5cc6f716792a
Jan Olszewski <janolszewski96@gmail.com>
2023-04-25 16:48:41 +0200

    Added User static create methods test

--- app/tests/Entity/UserTest.php
+++ app/tests/Entity/UserTest.php
@@ -3,6 +3,9 @@
 namespace App\Tests\Entity;
 
 use App\Entity\User;
+use League\OAuth2\Client\Provider\FacebookUser;
+use League\OAuth2\Client\Provider\GoogleUser;
+use League\OAuth2\Client\Provider\LinkedInResourceOwner;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
@@ -58,5 +61,71 @@ class UserTest extends KernelTestCase
         $this->assertSame($googleSubId, $user->getGoogleSubId());
         $this->assertSame($linkedInSubId, $user->getLinkedInSubId());
         $this->assertSame($facebookSubId, $user->getFacebookSubId());
+    }
+
+    public function testCreateGoogleUser(): void
+    {
+        $email = uniqid('email_').'@test.com';
+        $firstName = uniqid('firstName');
+        $lastName = uniqid('lastName');
+        $googleSubId = uniqid('googleSubId_');
+        $expectedUser = (new User())
+            ->setEmail($email)
+            ->setFirstName($firstName)
+            ->setLastName($lastName)
+            ->setGoogleSubId($googleSubId);
+
+        $googleUser = new GoogleUser([
+            'sub' => $googleSubId,
+            'given_name' => $firstName,
+            'family_name' => $lastName,
+            'email' => $email,
+        ]);
+
+        $this->assertEquals($expectedUser, User::createGoogleUser($googleUser));
+    }
+
+    public function testCreateLinkedInUser(): void
+    {
+        $email = uniqid('email_').'@test.com';
+        $firstName = uniqid('firstName');
+        $lastName = uniqid('lastName');
+        $linkedInSubId = uniqid('linkedInSubId_');
+        $expectedUser = (new User())
+            ->setEmail($email)
+            ->setFirstName($firstName)
+            ->setLastName($lastName)
+            ->setLinkedInSubId($linkedInSubId);
+
+        $linkedInUser = new LinkedInResourceOwner([
+            'localizedLastName' => $lastName,
+            'id' => $linkedInSubId,
+            'localizedFirstName' => $firstName,
+            'email' => $email,
+        ]);
+
+        $this->assertEquals($expectedUser, User::createLinkedInUser($linkedInUser));
+    }
+
+    public function testCreateFacebookUser(): void
+    {
+        $email = uniqid('email_').'@test.com';
+        $firstName = uniqid('firstName');
+        $lastName = uniqid('lastName');
+        $facebookSubId = uniqid('facebookSubId_');
+        $expectedUser = (new User())
+            ->setEmail($email)
+            ->setFirstName($firstName)
+            ->setLastName($lastName)
+            ->setFacebookSubId($facebookSubId);
+
+        $facebookUser = new FacebookUser([
+            'id' => $facebookSubId,
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'email' => $email,
+        ]);
+
+        $this->assertEquals($expectedUser, User::createFacebookUser($facebookUser));
     }
 }
