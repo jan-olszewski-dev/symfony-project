38424aee71a7b5635ecad299e9f718ec2b712cd3
Jan Olszewski <janolszewski96@gmail.com>
2023-05-08 16:01:47 +0200

    Handled images building

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -21,6 +21,10 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[ext]',
+    })
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
