9868d90193adf2d4c6cd8754e1c4595f90d5cace
Jan Olszewski <janolszewski96@gmail.com>
2023-05-23 11:03:56 +0200

    beautify pagination

--- app/assets/controllers/pagination_controller.js
+++ app/assets/controllers/pagination_controller.js
@@ -12,17 +12,24 @@ export default class PaginationController extends Controller {
 
     initialize() {
         this.page = 1
-        this.preparePageButtons();
         this.showCurrentPage();
     }
 
     preparePageButtons() {
+        const buttonsWrapper = this.pageButtonTarget.parentNode;
+        const newButton = this.pageButtonTarget.cloneNode(true);
+        const minPageButtonNumber = this.minPageButtonNumber;
+        const maxPageButtonNumber = this.maxPageButtonNumber;
+        buttonsWrapper.innerHTML = newButton.outerHTML;
+
         for (let i = this.maxPage; i >= 1; i--) {
-            const newButton = this.pageButtonTarget.cloneNode(true);
-            newButton.setAttribute('data-pagination-pagenumber-param', i);
-            newButton.innerText = i;
-            newButton.classList.remove('d-none');
-            this.pageButtonTarget.insertAdjacentHTML('afterend', newButton.outerHTML);
+            if (i >= minPageButtonNumber && i <= maxPageButtonNumber) {
+                newButton.setAttribute('data-pagination-pagenumber-param', i);
+                newButton.innerText = i;
+                newButton.classList.remove('d-none', 'btn-primary', 'btn-secondary');
+                this.page === i ? newButton.classList.add('btn-primary') : newButton.classList.add('btn-secondary');
+                this.pageButtonTarget.insertAdjacentHTML('afterend', newButton.outerHTML);
+            }
         }
     }
 
@@ -33,6 +40,7 @@ export default class PaginationController extends Controller {
         this.addressTargets.forEach((element, index) => {
             element.hidden = index < visibleMinIndex || index >= visibleMaxIndex
         });
+        this.preparePageButtons();
     }
 
     showPage(event) {
@@ -60,5 +68,16 @@ export default class PaginationController extends Controller {
 
     get maxPage() {
         return Number((this.addressTargets.length / this.perPageValue).toPrecision(1));
+    }
+
+    get minPageButtonNumber() {
+        return this.page > 0 && this.page < 3 ?
+            1 : this.page - 2;
+    }
+
+    get maxPageButtonNumber() {
+        return this.page > 0 && this.page < 3 ?
+            (this.maxPage > 5 ? 5 : this.maxPage) :
+            this.page + 2;
     }
 }
